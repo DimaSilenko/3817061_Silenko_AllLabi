c9966a70bf89ff81fccae28d168d8bed8b954bb0
DimaSilenko <43129870+DimaSilenko@users.noreply.github.com>
2018-11-03 23:11:06 +0300

    Взятки сделаны, взяток больше нет

--- Vector/Main.cpp
+++ Vector/Main.cpp
@@ -12,13 +12,16 @@ void main()
 	TVector<int> ivec2(vec1, 3);
 	cout << "Стандартный целочисленный вектор:\n" << ivec2;
 	TVector<int> ivec3(ivec2);
+	TVector<int> temp;
+	int tmp;
 met1:
 
 	cout << "Введите целочисленный вектор и с базовым вектором произойдут некоторые арифметические операции\n";
 	cin >> ivec1;
 	try
 	{
-		cout << "Сложение:\n" << ivec3 + ivec1 << "\n";
+		temp = ivec1 + ivec3;
+		cout << "Сложение:\n" << temp << "\n";
 	}
 	catch (int err)
 	{
@@ -28,18 +31,21 @@ met1:
 			goto met1;
 		}
 	}
-	cout << "Вычитание:\n" << ivec3 - ivec1 << "\n";
-	cout << "Умножение скалярное:\n" << ivec3 * ivec1 << "\n";
+	temp = ivec3 - ivec1;
+	cout << "Вычитание:\n" << temp << "\n";
+	tmp = ivec3 * ivec1;
+	cout << "Умножение скалярное:\n" << tmp << "\n";
 	cout << "Введите число и посмотрите на аналогичный результат\n";
 	cin >> n;
-	cout << "Умножение на число:\n" << ivec3 * n << "\n";
-	cout << "Деление на число:\n" << ivec3 / n << "\n";
+	temp = ivec3 * n;
+	cout << "Умножение на число:\n" << temp << "\n";
+	temp = ivec3 / n;
+	cout << "Деление на число:\n" << temp<< "\n";
 
 	float fvec[3] = { 1.1, 2.4, 3.4 };
 	TVector<float> fvec1(fvec, 3);
 	cout << "Стандартный вещественный вектор:\n" << fvec1;
 	TVector<float> fvec2;
-
 met2:
 	cout << "Введите вещественный вектор\n";
 	cin >> fvec2;
